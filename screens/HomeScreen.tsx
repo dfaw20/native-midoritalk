d011f921487ce7469bfc3deff9ef71c6848c65c6
dfaw20 <dfaw200@gmail.com>
2023-02-12 12:17:49 +0900

    feature/local_character

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,9 +1,5 @@
-import { StyleSheet } from 'react-native';
-
-import { Text, View } from '../components/Themed';
+import { View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
-import ContactList from "../components/ContactList";
-import {loadCharacters} from "../repository/SampleRepository";
 
 export default function HomeScreen({ navigation }: RootTabScreenProps<'Home'>) {
   return (
